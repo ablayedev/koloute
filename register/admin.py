69529c7b54f4b83ec7d9e4643260679f112ecbd1
ablayedev <37193242+ablayedev@users.noreply.github.com>
2020-01-06 13:31:30 +0000

    versionèt_pro

--- register/admin.py
+++ register/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from register.models import Station,Box,Location
 # Register your models here.
-admin.site.register(Station)
-admin.site.register(Box)
-admin.site.register(Location)
+#admin.site.register(Station)
+#admin.site.register(Box)
+#admin.site.register(Location)
--- utilisateur/urls.py
+++ utilisateur/urls.py
@@ -3,7 +3,7 @@ from utilisateur import views
 app_name="utilisateur"
 urlpatterns = [
     path('', views.home,name="home"),
-    path('admin-tolbi', views.admin,name="admin"),
+    #path('admin-tolbi', views.admin,name="admin"),
     path('logout/',views.deconect,name="logout")
   
 ]
